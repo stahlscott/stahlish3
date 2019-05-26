8ea594a2f3299389adb1e67926c79c6c4b7cd891
Scott Stahl <scott@skiplist.com>
2019-05-26 11:55:05 -0400

    goog analytics

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -35,6 +35,15 @@ const TemplateWrapper = ({ children }) => (
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/" />
           <meta property="og:image" content="/img/og-image.jpg" />
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-75783400-1"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-75783400-1');
+            </script>
         </Helmet>
         <Navbar />
         <div>{children}</div>
